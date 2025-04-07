6acb1943d4c1a2d8965db659d79ab0eef832982b
Phantather <akdil.hstech@gmail.com>
2025-04-07 23:40:16 +0600

    first commit

---
+++ src/js/tailwind.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#6C3C10',
+                primaryBlack: '#000000',
+            }
+        },
+        screens: {
+            md: { max: '1024px' },
+            sm: { max: '768px' },
+            xs: { max: '568px' },
+        },
+    }
+}
