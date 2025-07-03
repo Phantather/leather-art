fe3d97b84ae20589f6a2cc8f75241225c2356d1e
Phantather <akdil.hsech@gmail.com>
2025-07-03 15:03:38 +0600

    fix middleware config

--- middleware.js
+++ middleware.js
@@ -3,10 +3,7 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request) {
     const response = NextResponse.next();
-
-    // Удаляем поддержку Range-запросов
     response.headers.set('Accept-Ranges', 'none');
     response.headers.set('Cache-Control', 'public, max-age=3600');
-
     return response;
 }
